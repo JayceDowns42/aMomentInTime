0e29f51f058c4beab50c40c27c30355c7b36feae
JayceDowns42 <jaycedowns42@gmail.com>
2019-03-24 21:41:31 -0500

    added code
    added code

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
 const chalk = require('chalk');
 var moment = require('moment');
 
-console.log(chalk.blue("Hello World!"));
+// console.log(chalk.blue.underline(moment().format('dddd')));
+
+if (moment().format('HH') < 12) {
+    console.log(chalk.yellow.underline(moment().format('dddd')));
+    console.log(chalk.yellow.underline('Good Morning!'));
+} else if (moment().format('HH') >= 12 && moment().format('HH') < 17) {
+    console.log(chalk.red.underline(moment().format('dddd')));
+    console.log(chalk.red.underline('Good Afternoon!'));
+} else {
+    console.log(chalk.blue.underline(moment().format('dddd')));
+    console.log(chalk.blue.underline('Good Evening!'));
+}
